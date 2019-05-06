5905c8014faad1c0b7271a132061753e404821fc
Petr Snobelt <petr.snobelt@kiwi.com>
2019-05-06 11:41:53 +0200

    move custom hook inside own file

---
+++ hooks/useFocus.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+const useFocus = () => {
+  const [state, setState] = useState(null);
+  const onFocusEvent = event => {
+    setState(true);
+  };
+  const onBlurEvent = event => {
+    setState(false);
+  };
+  useEffect(() => {
+    window.addEventListener("focus", onFocusEvent);
+    window.addEventListener("blur", onBlurEvent);
+    return () => {
+      window.removeEventListener("focus", onFocusEvent);
+      window.removeEventListener("blur", onBlurEvent);
+    };
+  });
+  return state;
+};
+
+export default useFocus;
